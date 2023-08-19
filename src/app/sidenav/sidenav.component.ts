fc0f8a4d53c8e69a123f233d1277bd8cb5f2b9e2
JorgeHCamara <jorgehcamara@hotmail.com>
2023-08-19 02:57:17 -0300

    chore: navbar part 2

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -1,5 +1,6 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, OnInit, HostListener } from '@angular/core';
 import { navbarData } from './nav-data';
+import { animate, style, transition, trigger, keyframes } from '@angular/animations'
 
 interface SideNavToggle {
   screenWidth: number;
@@ -9,15 +10,55 @@ interface SideNavToggle {
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
-  styleUrls: ['./sidenav.component.scss']
+  styleUrls: ['./sidenav.component.scss'],
+  animations: [
+    trigger('fadeInOut', [
+      transition(':enter', [
+        style({opacity: 0}),
+        animate('350ms',
+          style({opacity: 1})
+        )
+      ]),
+      transition(':leave', [
+        style({opacity: 1}),
+        animate('350ms',
+          style({opacity: 0})
+        )
+      ])
+    ]),
+    trigger('rotate', [
+      transition(':enter', [
+        animate('1000ms',
+          keyframes([
+            style({transform: 'rotate(0deg)', offset: '0'}),
+            style({transform: 'rotate(2turn)', offset: '1'}),
+          ])
+        )
+      ])
+    ])
+
+  ]
 })
-export class SidenavComponent {
+export class SidenavComponent implements OnInit {
 
 
   @Output() onToggleSideNav: EventEmitter<SideNavToggle> = new EventEmitter();
   collapsed = false;
   screenWidth = 0;
   navData = navbarData;
+
+  @HostListener('window: resize', ['$event'])
+  onResize(event: any) {
+    this.screenWidth = window.innerWidth;
+    if(this.screenWidth <= 768) {
+      this.collapsed = false;
+      this.onToggleSideNav.emit({collapsed: this.collapsed, screenWidth: this.screenWidth})
+    }
+  }
+
+  ngOnInit() {
+    this.screenWidth = window.innerWidth;
+  }
 
   toggleCollapse(): void {
     this.collapsed = !this.collapsed;
