1dd552105fae0329c602d4e7777721f8a6853818
JorgeHCamara <jorgehcamara@hotmail.com>
2023-08-19 13:18:29 -0300

    chore: change from media to weather component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { ProductsComponent } from './products/products.component';
 import { StatisticsComponent } from './statistics/statistics.component';
 import { CoupensComponent } from './coupens/coupens.component';
 import { PagesComponent } from './pages/pages.component';
-import { MediaComponent } from './media/media.component';
+import { WeatherComponent } from './weather/weather.component';
 import { SettingsComponent } from './settings/settings.component';
 
 const routes: Routes = [
@@ -15,7 +15,7 @@ const routes: Routes = [
   {path: 'statistics', component: StatisticsComponent},
   {path: 'coupens', component: CoupensComponent},
   {path: 'pages', component: PagesComponent},
-  {path: 'media', component: MediaComponent},
+  {path: 'weather', component: WeatherComponent},
   {path: 'settings', component: SettingsComponent},
 ];
 
