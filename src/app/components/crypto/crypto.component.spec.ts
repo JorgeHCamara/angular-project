c0097c2944ce651379bd324dbeaeedb18ea5b504
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-24 11:33:46 -0300

    chore: change from pages component to crypto component

--- src/app/components/crypto/crypto.component.spec.ts
+++ src/app/components/crypto/crypto.component.spec.ts
@@ -1,16 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { PagesComponent } from './pages.component';
+import { CryptoComponent } from './crypto.component';
 
-describe('PagesComponent', () => {
-  let component: PagesComponent;
-  let fixture: ComponentFixture<PagesComponent>;
+describe('CryptoComponent', () => {
+  let component: CryptoComponent;
+  let fixture: ComponentFixture<CryptoComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [PagesComponent]
+      declarations: [CryptoComponent]
     });
-    fixture = TestBed.createComponent(PagesComponent);
+    fixture = TestBed.createComponent(CryptoComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
